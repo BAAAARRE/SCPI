0c52fe447123d1a41e0257614daafb0f7716df17
BAAAARRE <75239652+BAAAARRE@users.noreply.github.com>
2021-01-02 12:03:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import json
 from bs4 import BeautifulSoup
 import plotly.graph_objects as go
 import plotly.express as px
@@ -11,8 +10,8 @@ def main():
 
 # Set configs
     st.set_page_config(
-	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-	initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
 	page_title='SCPI',  # String or None. Strings get appended with "• Streamlit". 
 	page_icon=None,  # String, anything supported by st.image, or None.
     )
@@ -22,19 +21,34 @@ def main():
     info, prix, distri = scrap(275)
     prix = clean_prix(prix)
     distri = clean_distri(distri)
+    #df = pd.merge(prix,distri,  how='inner', on = ['SCPI', 'Année'])
+    info = clean_info(info)
 
 # Set Sidebar
     st.sidebar.title('Navigation onglet')
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Ranking", "General"])
 
-# Configure generals filters
-
-    
 # Page 1
     if page == "Homepage":
-        st.dataframe(info)
-        st.dataframe(prix)
-        st.dataframe(distri)
+
+        # Set Sidebar
+        st.sidebar.title('Generals filters')
+        sel_capital = st.sidebar.multiselect('Select Capital', sorted(info['Capital'].unique()))
+        slider_distri = st.sidebar.slider('Taux de distribution', info['Tx distrib'].min(), info['Tx distrib'].max(), (info['Tx distrib'].min(), info['Tx distrib'].max()))
+        slider_occup = st.sidebar.slider("Taux d'occupation", info['Tx occup'].min(), info['Tx occup'].max(), (info['Tx occup'].min(), info['Tx occup'].max()))
+        slider_crea = st.sidebar.slider('Année de Création', int(info['Date de création'].min()), int(info['Date de création'].max()), (int(info['Date de création'].min()), int(info['Date de création'].max())))
+        # Configure generals filters
+        df_capital = multi_filter(info, sel_capital, 'Capital')
+        df_distri = info[info['Tx distrib'].between(slider_distri[0],slider_distri[1])]
+        df_occup = info[info['Tx occup'].between(slider_occup[0],slider_occup[1])]
+        df_crea = info[info['Date de création'].between(slider_crea[0],slider_crea[1])]
+
+        info_select = info[info.isin(df_capital) & info.isin(df_distri) & info.isin(df_occup) & info.isin(df_crea)].dropna()
+
+        st.dataframe(info_select, width=2024, height=768)
+        #st.dataframe(prix)
+        #st.dataframe(distri)
+        #st.dataframe(data)
 	
 # Bottom page
     st.write("\n") 
@@ -54,7 +68,7 @@ def all_element(typ, detail, uri):
 
 @st.cache
 def scrap(nb_sites):
-    info = {'SCPI': [],'Capital': [], 'Date de création': [], 'Nb Associés': [], 'Nb Parts': [], 'Tx occup': [], 'Frais Souscription': []}
+    info = {'SCPI': [],'Capital': [], 'Date de création': [], 'Tx distrib': [], 'Prix souscri': [], 'Nb Associés': [], 'Nb Parts': [], 'Tx occup': [], 'Frais Souscription': []}
     lis_prix = []
     lis_distri = []
 
@@ -66,39 +80,60 @@ def scrap(nb_sites):
         title = soup.find('h1').string
 
         if title != '404 Page Not Found' and title[:4] != 'OPCI':
+            
+            year = all_element('span','bl left clear ml1 txtgrey txt11 txtitalic', url)[0][-5:-1]
+            if '/' not in year:
+                if int(year)>2019:
 
-            # Info
-            scpi = title[5:]
-            info['SCPI'].append(scpi)
-            info_gen = all_element('span','w40 txtlight txtleft', url)
-            info['Capital'].append(info_gen[2])
-            info['Date de création'].append(info_gen[3])
-            info['Nb Associés'].append(info_gen[6])
-            info['Nb Parts'].append(info_gen[7])
+                    # Info
+                    chiffre = all_element('span','bl left clear txtgrey txt36 txtlight', url)
+                    info['Tx distrib'].append(chiffre[0][1:-1])
+                    info['Prix souscri'].append(chiffre[1][1:-1])
 
-            info_det = all_element('span','w30 txtlight txtright', url)
-            info['Tx occup'].append(info_det[4])
+                    scpi = title[5:]
+                    info['SCPI'].append(scpi)
+                    info_gen = all_element('span','w40 txtlight txtleft', url)
+                    info['Capital'].append(info_gen[2])
+                    info['Date de création'].append(info_gen[3])
+                    info['Nb Associés'].append(info_gen[6])
+                    info['Nb Parts'].append(info_gen[7])
 
-            info_frais = all_element('span','w40 txtlight txtright', url)
-            info['Frais Souscription'].append(info_frais[0])
+                    info_det = all_element('span','w30 txtlight txtright', url)
+                    info['Tx occup'].append(info_det[4])
 
-            # Tables
-            df_list = pd.read_html(html)
+                    info_frais = all_element('span','w40 txtlight txtright', url)
+                    info['Frais Souscription'].append(info_frais[0])
 
-            # Prix
-            df_prix = df_list[1]
-            df_prix['SCPI'] = scpi
-            lis_prix.append(df_prix)
+                    # Tables
+                    df_list = pd.read_html(html)
 
-            # Distribution
-            df_distri = df_list[3]
-            df_distri['SCPI'] = scpi
-            lis_distri.append(df_distri)   
+                    # Prix
+                    df_prix = df_list[1]
+                    df_prix['SCPI'] = scpi
+                    lis_prix.append(df_prix)
 
-    info = pd.DataFrame(info)
-    prix = pd.concat(lis_prix)
-    distri = pd.concat(lis_distri)
+                    # Distribution
+                    df_distri = df_list[3]
+                    df_distri['SCPI'] = scpi
+                    lis_distri.append(df_distri)   
+
+    info = pd.DataFrame(info).dropna()
+    prix = pd.concat(lis_prix)[['Année', 'Prix acquéreur *', 'Dividende par part', 'Résultat Courant','RAN / part', 'SCPI']].dropna()
+    distri = pd.concat(lis_distri)[['Année', 'Taux de distribution**', 'Variation du prix ***', 'SCPI']].dropna()
     return info, prix, distri
+
+def clean_info(info):
+    clean_info = info.copy()
+    clean_info = clean_info[clean_info['Tx distrib'] != 'NC'].reset_index().drop('index', axis = 1)
+    for i in ['Tx distrib', 'Prix souscri','Nb Associés', 'Nb Parts', 'Tx occup', 'Frais Souscription']:
+        clean_info[i] = clean_info[i].str.replace(r'-', '0')
+        clean_info[i] = clean_info[i].str.replace(r'%', '0')
+        clean_info[i] = clean_info[i].str.replace(' ', '')
+        clean_info[i] = clean_info[i].str.replace(',', '.').astype(float).round(2)
+    clean_info['Date de création'] = clean_info['Date de création'].str.replace(r'-', '0')
+    clean_info['Date de création'] = clean_info['Date de création'].str[-4:].astype(int)
+    
+    return clean_info
 
 def clean_prix(prix):
     clean_prix = prix[['SCPI','Année','Prix acquéreur *','Dividende par part','Résultat Courant','RAN / part']]
@@ -108,7 +143,6 @@ def clean_prix(prix):
         clean_prix[i] = clean_prix[i].str.replace(r'€', '')
         clean_prix[i] = clean_prix[i].str.replace(' ', '')
         clean_prix[i] = clean_prix[i].str.replace(',', '.').astype(float)
-    clean_prix = clean_prix.fillna(0)
     return clean_prix
 
 def clean_distri(distri):
@@ -118,8 +152,9 @@ def clean_distri(distri):
         clean_distri[i] = clean_distri[i].str.replace(r'%', '')
         clean_distri[i] = clean_distri[i].str.replace(' ', '')
         clean_distri[i] = clean_distri[i].str.replace(',', '.').astype(float)
-    clean_distri = clean_distri.fillna(0)
     return clean_distri
+
+
 
 def multi_filter(df, sel, var):
     if len(sel) == 0:
